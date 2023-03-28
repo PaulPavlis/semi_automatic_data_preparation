5e3077e86e337087ce82f7baaa85c81259174f81
Paul Pavlis <wi18b025@technikum-wien.at>
2023-03-28 14:31:41 +0200

    added basic setup

--- flask_controller.py
+++ flask_controller.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
 
-
-# Defining the home page of our site
-@app.route("/")  # this sets the route to this page
+@app.route("/")
+@app.route("/home")
 def home():
-    return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
+    return render_template("index.html")
+
+
+@app.route("/import_data")
+def import_data():
+    return "<h1>Fill me in</h1>"
+
+
+@app.route("/data_exploration_home")
+def data_exploration_home():
+    return "<h1>Fill me in</h1>"
+
+
+@app.route("/data_preparation_home")
+def data_preparation_home():
+    return "<h1>Fill me in</h1>"
+
+
+@app.route("/machine_learning")
+def machine_learning():
+    return "<h1>Fill me in</h1>"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
