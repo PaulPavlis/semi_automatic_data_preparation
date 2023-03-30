fd7091cf9cbaa09b39445cd997ff4abb153badef
Paul Pavlis <wi18b025@technikum-wien.at>
2023-03-30 16:27:14 +0200

    Added display tables

--- static/js/my_js.js
+++ static/js/my_js.js
@@ -2,3 +2,8 @@
 setTimeout(function () {
     $(".flashed_messages").fadeOut("slow");
 }, 5000); // <-- time in milliseconds
+
+// Display tables functionality
+$(document).ready(function () {
+    $(".display_table").DataTable();
+});
